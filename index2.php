6d679eeaba3a421cce78accadba33b9b5f8579f0
joddie <jhoelguanotoa@gmail.com>
2023-10-30 16:14:26 -0500

    first commit

---
+++ index2.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OPERACIONES</title>
+
+     <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="dist/css/adminlte.min.css">
+
+</head>
+<body>
+ <div class="row">  
+  <div class="col-sm-6"> 
+
+    <!-- /.card -->
+            <!-- Horizontal Form -->
+            <div class="card card-info">
+              <div class="card-header">
+                <h3 class="card-title">OPERACIONES</h3>
+              </div>
+              <!-- /.card-header -->
+              <!-- form start -->
+              <form class="form-horizontal">
+                <div class="card-body">
+                  <div class="form-group row">
+                    <label for="inputEmail3" class="col-sm-10 col-form-label">Primer Numero:</label>
+                    <div class="col-sm-5">
+                      <input type="primer-numero" class="form-control" id="inputEmail3" placeholder="Primer Numero">
+                    </div>
+                  </div>
+                  <div class="form-group row">
+                    <label for="inputPassword3" class="col-sm-5 col-form-label">Segundo Numero:</label>
+                    <div class="col-sm-7.5">
+                      <input type="segundo numero" class="form-control" id="inputPassword3" placeholder="Segundo Numero">
+                    </div>
+                  </div>
+                  <div class="form-group row">
+                    <label for="inputEmail3" class="col-sm-5 col-form-label">Resultado:</label>
+                    <div class="col-sm-7.5">
+                      <input type="resultado" class="form-control" id="inputResultado3" placeholder="Resultado">
+                    </div>
+                  </div>
+                  
+                  
+                  
+                </div>
+                <!-- /.card-body -->
+                <div class="card-footer">
+                  <button type="submit" class="btn btn-info">Calcular</button>
+                  <button type="submit" class="btn btn-default float-right">Salir</button>
+                </div>
+                <!-- /.card-footer -->
+              </form>
+            </div>
+    </div>
+    <div class="col-sm-6">
+            <!-- Horizontal Form -->
+            <div class="card card-info">
+              <div class="card-header">
+                <h3 class="card-title">OPERACIONES</h3>
+              </div>
+              <!-- /.card-header -->
+              <!-- form start -->
+              <form class="form-horizontal">
+                <div class="card-body">
+                  <div class="form-group row">
+                    <label for="inputEmail3" class="col-sm-5 col-form-label">Primer Numero:</label>
+                    <div class="col-sm-7.5">
+                      <input type="primer-numero" class="form-control" id="inputEmail3" placeholder="Primer Numero">
+                    </div>
+                  </div>
+                  <div class="form-group row">
+                    <label for="inputPassword3" class="col-sm-5 col-form-label">Segundo Numero:</label>
+                    <div class="col-sm-7.5">
+                      <input type="segundo numero" class="form-control" id="inputPassword3" placeholder="Segundo Numero">
+                    </div>
+                  </div>
+                  <div class="form-group row">
+                    <label for="inputEmail3" class="col-sm-5 col-form-label">Resultado:</label>
+                    <div class="col-sm-7.5">
+                      <input type="resultado" class="form-control" id="inputResultado3" placeholder="Resultado">
+                    </div>
+                  </div>
+                  
+                  
+                  
+                </div>
+                <!-- /.card-body -->
+                <div class="card-footer">
+                  <button type="submit" class="btn btn-info">Calcular</button>
+                  <button type="submit" class="btn btn-default float-right">Salir</button>
+                </div>
+                <!-- /.card-footer -->
+              </form>
+            </div>
+  </div>            
+ </div>  
+</body>
+</html>
